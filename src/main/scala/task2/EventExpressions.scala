e80b030b329b0e3448f10ddcf484f0b295760035
ragnar <ragnar@rmgk.de>
2021-08-22 17:31:28 +0200

    event expressions task

--- src/main/scala/task2/EventExpressions.scala
+++ src/main/scala/task2/EventExpressions.scala
@@ -6,101 +6,107 @@ import scala.collection.immutable.Queue
 
 object EventExpressions {
 
-  def main(args: Array[String]): Unit = {}
+  val a = Evt[String]
+  val b = Evt[String]
 
+  val s0 = Evt[String]
+  val s1 = Var("init s1")
+  val s2 = Var("init s2")
 
-  def eventCombinators(): Unit = {
-    val a = Evt[String]
-    val b = Evt[String]
+  val v1 = Var("Initial")
+  val v2 = Var(v1)
 
-    {
-      val c = a || b
-      val d = Event { if(a.value.isDefined) a.value else b.value }
-      val e = Event { a.value.orElse(b.value) }
-    }
+  val n = 5
 
-    {
-      val c = a \ b
-      val d = Event { if(b.value.isEmpty) a.value else None }
-    }
+  val c1  = a || b
+  val c2  = a \ b
+  val c3  = a zip b
+  val c4  = a zipOuter b
+  val c5  = a.dropParam
+  val c6  = a.filter(_.startsWith("x"))
 
-    {
-      val c = a zip b
-      val d = Event { a.value zip b.value }
-    }
+  val c7  = s0.count()
+  val c8  = s0.latest("init")
+  val c9  = s0.latestOption()
+  val c10 = s0.list()
+  val c11 = s0.last(n)
+  val c12 = s0.toggle(s1, s2)
+  val c13 = v2.flatten
 
-    {
-      val c = a zipOuter b
-      val d = Event { if(a.value.isDefined || b.value.isDefined) Some(a.value -> b.value) else None}
-    }
+  val d1  = Event { if (a.value.isDefined && a.value.get.startsWith("x")) a.value else None }
+  val d2  = Event { a.value zip b.value }
+  val d3  = Event { if (a.value.isDefined) a.value else b.value }
+  val d4  = Event { if (a.value.isDefined || b.value.isDefined) Some(a.value -> b.value) else None }
+  val d5  = Event { if (b.value.isEmpty) a.value else None }
+  val d6  = Event { if (a.value.isDefined) Some(()) else None }
 
-    {
-      val c = a.dropParam
-      val d = a.map(_ => ())
-      val e = Event{ if (a.value.isDefined) Some(()) else None }
-    }
-
-    {
-      val c = a.filter(_.startsWith("x"))
-      val d = Event { a.value.filter(_.startsWith("x")) }
-      val e = Event { if (a.value.isDefined && a.value.get.startsWith("x")) a.value else None}
-    }
+  val d7  = s0.fold(List.empty[String]) { (list, v) => v :: list }
+  val d8  = s0.fold(Option.empty[String])((_, v) => Some(v))
+  val d9  = s0.fold(0)((s, _) => s + 1)
+  val d10 = s0.fold(Queue.empty[String]) { (queue, v) =>
+    if (queue.length >= n) queue.tail.enqueue(v) else queue.enqueue(v)
   }
+  val d11 = s0.fold("init")((_, v) => v)
+  val d12 = { val intermediate = s0.iterate(true)(v => !v); Signal { if (intermediate.value) s1.value else s2.value } }
+  val d13 = Signal.dynamic { v2.value.value }
 
-  def foldCombinators(): Unit = {
+  val e1  = Event { a.value.filter(_.startsWith("x")) }
+  val e2  = a.map(_ => ())
+  val e3  = Event { a.value.orElse(b.value) }
 
-    val a = Evt[String]
+  val e7  = s0.iterate(0)(s => s + 1)
 
-    {
-      val c = a.count()
-      val d = a.iterate(0)(s => s + 1)
-      val e = a.fold(0)((s, _) => s + 1)
-    }
-
-    {
-      val c = a.latest("init")
-      val d = a.fold("init")((_, v) => v)
-    }
-
-    {
-      val c = a.latestOption()
-      val d = a.fold(Option.empty[String])((_, v) => Some(v))
-    }
-
-    {
-      val c = a.list()
-      val d = a.fold(List.empty[String]){ (list, v) => v :: list }
-    }
-
-    {
-      val n = 5
-      val c = a.last(n)
-      val d = a.fold(Queue.empty[String]) {  (queue, v) =>
-        if (queue.length >= n) queue.tail.enqueue(v) else queue.enqueue(v)
+  def assertEquals[T >: Null](c: Event[T], d: Event[T]): Unit = {
+    var count = 2
+    var ct: T = null
+    var dt: T = null
+    def check = {
+      count -= 1
+      if (count == 0) {
+        if (ct == dt) println("OK")
+        else println(s"events differ:\nevent 1: $c\nevent 2:$d")
+        count = 2
       }
     }
-
-    {
-      val s1 = Var("init s1")
-      val s2 = Var("init s2")
-
-      val c = a.toggle(s1, s2)
-      val d = {
-        val intermediate = a.iterate(true)(v => !v)
-        Signal { if (intermediate.value) s1.value else s2.value}
-      }
-    }
-
+    c.observe { v => ct = v; check }
+    d.observe { v => dt = v; check }
   }
 
-  def flattenCombinators(): Unit = {
-    val a = Var("Initial")
-    val b = Var(a)
+  def assertEquals[T >: Null](c: Signal[T], d: Signal[T]): Unit = {
+    var count = 2
+    var ct: T = null
+    var dt: T = null
+    def check = {
+      count -= 1
+      if (count == 0) {
+        if (ct == dt) println("OK2")
+        else println(s"signals differ:\nsignal 1: $c\nsignal 2:$d")
+        count = 2
+      }
+    }
+    c.observe { v => ct = v; check }
+    d.observe { v => dt = v; check }
+  }
 
-    val c = b.flatten
-    val d = Signal.dynamic { b.value.value }
+  def main(args: Array[String]): Unit = {
+    a.fire("hi")
+    b.fire("ho")
+    transaction(a, b) { implicit t =>
+      a.fire("trans hi")
+      b.fire("trans ho")
+    }
+    a.fire("hi 2")
+    a.fire("hi 3")
 
-    assert(c.now == "Initial")
+    s0.fire("ev 1")
+    s0.fire("ev 2")
+    s1.set("change 1")
+    s2.set("change 2")
+    s0.fire("ev 3")
+    s0.fire("ev 4")
+    s1.set("change 3")
+    s0.fire("ev 5")
+
   }
 
 }
